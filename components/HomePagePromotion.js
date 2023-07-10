745b9f6c653a2e251890945ef02d04fa5d9edf69
Purnoy <purnoy.cc@gmail.com>
2023-07-10 13:06:46 +0600

    new commit

--- components/HomePagePromotion.js
+++ components/HomePagePromotion.js
@@ -18,9 +18,9 @@ const HomePagePromotion = () => {
 
             <div className="w-[100%] md:w-[50%] p-6 ms-12 flex items-center  md:items-start justify-between flex-col">
                 <h1 className="  text-[#354895] font-bold text-center md:text-left text-[35px] mb-4">
-                    Shikho'র সাথেই শিখছে <br/>সবাই, জিতছে সবাই
+                    Shikhoর সাথেই শিখছে <br/>সবাই, জিতছে সবাই
                 </h1>
-                <p className="text-[#354895] text-center md:text-left">এখন তুমি যেখানেই থাকো, ম্যাটার করে না কিছুই! কারণ সবাই শিখছে আর জিতছে দেশের সবচেয়ে নির্ভরযোগ্য ডিজিটাল লার্নিং প্রতিষ্ঠান শিখো'র সাথে। কিন্তু কীভাবে? জানতে পাশে ভিডিওটি দেখে চঞ্চল চৌধুরীর কাছ থেকেই জেনে নাও!</p>
+                <p className="text-[#354895] text-center md:text-left">এখন তুমি যেখানেই থাকো, ম্যাটার করে না কিছুই! কারণ সবাই শিখছে আর জিতছে দেশের সবচেয়ে নির্ভরযোগ্য ডিজিটাল লার্নিং প্রতিষ্ঠান শিখোর সাথে। কিন্তু কীভাবে? জানতে পাশে ভিডিওটি দেখে চঞ্চল চৌধুরীর কাছ থেকেই জেনে নাও!</p>
                 <button type='button' className=' hidden  md:block mt-8 text-[16px]  py-4 shadow-lg hover:shadow-sm transition duration-200 w-[300px]  rounded-2xl b  mb-4 md:mb-0  hover: bg-[#475dff] text-white hover:bg-[#d63384]'>
                         
                         আমাদের সম্পর্কে জেনে নাও
