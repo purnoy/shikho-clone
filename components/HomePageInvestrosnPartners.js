f7a66064df692695571976d7d694ec37eaa911ee
Purnot <purnoy.cc@gmail.com>
2023-07-14 22:11:48 +0600

    slick slider responsive issue solved

--- components/HomePageInvestrosnPartners.js
+++ components/HomePageInvestrosnPartners.js
@@ -15,19 +15,21 @@ const HomePageInvestrosnPartners = () => {
         speed: 2000,
         autoplaySpeed: 2000,
         cssEase: "linear",
+        nextArrow: false,
+        prevArrow: false,
         responsive: [
             {
                 breakpoint: 1000,
                 settings: {
   
-                  slidesToShow: 2,
-                  slidesToScroll: 2,
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
                   infinite: true,
                 },
                 breakpoint: 600,
                 settings: {
   
-                  slidesToShow: 1,
+                  slidesToShow: 2,
                   slidesToScroll: 1,
                   infinite: true,
                 }
@@ -35,37 +37,33 @@ const HomePageInvestrosnPartners = () => {
         ]
     }
   return (
-    <div className="bg-[#eef1ff]">
-        <div className="section-intro">
-        <h1 className="text-[#354895] text-center font-semibold mb-16 text-[22px]">আমাদের ইনভেস্টরস</h1>
-            <Slider {...settings}>
-            
+    <div className="bg-[#eef1ff] py-16 px-8 w-[100vw]">
+        <div className=" mx-auto relative  py-8 px-4">
+            <div className=" w-[100%] mx-auto ">
+                <h1 className="text-[#354895] text-center font-semibold mb-16 text-[22px]">আমাদের ইনভেস্টরস</h1>
+                <Slider {...settings}>
                     {
                         investors.map(item=>{
                             return(
                                 <div className="" key={item.id}>
-
                                 <div className=" w-[80%] h-[150px] border-2 bg-white p-2 rounded-2xl mb-6 cursor-pointer" >
                                     <Image className="w-[100%] object-cover h-[100%] mb-8" src={item.image} height={300} width={300} alt="News Coverage Image"></Image>
                                 </div>
-                            
-                                
                             </div>
                             )
                         })
                     }
-            
             </Slider>
+            </div>
         </div>
-        <div className="section-intro">
+    <div className="mx-auto relative  py-8 px-4">
+      <div className=" w-[100%] mx-auto ">
         <h1 className="text-[#354895] text-center font-semibold mb-16 text-[22px]">আমাদের পার্টনারস</h1>
             <Slider {...settings}>
-            
                     {
                         partners.map(item=>{
                             return(
                                 <div className="" key={item.id}>
-
                                 <div className=" w-[80%] h-[150px] border-2 bg-white p-2 rounded-2xl mb-6 cursor-pointer" >
                                     <Image className="w-[100%] object-cover h-[100%] mb-8" src={item.image} height={300} width={300} alt="News Coverage Image"></Image>
                                 </div>
@@ -75,8 +73,8 @@ const HomePageInvestrosnPartners = () => {
                             )
                         })
                     }
-            
             </Slider>
+        </div>
         </div>
     </div>
   )
--- components/HomePageMediaCoverage.js
+++ components/HomePageMediaCoverage.js
@@ -6,14 +6,15 @@ import Link from "next/link";
 
 const HomePageMediaCoverage = () => {
     const settings = {
-        className: "center",
-        centerMode: true,
-        infinite: true,
-       
+        
+        infinite: true,      
         slidesToShow: 3,
+        slidesToScroll: 1,
         speed: 500,
         autoplay: true,
         dots: true,
+        nextArrow: false,
+        prevArrow: false,
         responsive: [
             {
               breakpoint: 1000,
@@ -34,29 +35,29 @@ const HomePageMediaCoverage = () => {
            ]
       };
   return (
-    <div className="bg-[#eef1ff] section-intro ">
+    <div className="bg-[#eef1ff] py-16 px-8 w-[100vw]">
       <div className="container mb-8 mx-auto">
         <h1 className="text-[#354895] text-center font-semibold mb-4 text-[22px]">গণমাধ্যমে Shikho</h1>
-        <p className="text-[#354895] text-center text-[14px]">দৈনিক সংবাদপত্র, টেলিভিশন চ্যানেলসহ বিভিন্ন গণমাধ্যমে Shikho<br/> সম্পর্কে প্রকাশিত বিভিন্ন সংবাদ ও প্রতিবেদনসমূহ</p>
+        <p className="text-[#354895] text-center text-[14px]">দৈনিক সংবাদপত্র, টেলিভিশন চ্যানেলসহ বিভিন্ন গণমাধ্যমে Shikho<br/> সম্পর্কে প্রকাশিত বিভিন্ন সংবাদ ও প্রতিবেদনসমূহ</p>   
       </div>
-      
-    <div className="">
-        <Slider {...settings} className="overflow-hidden">
+      <div className=" mx-auto relative  py-8 px-4">
+      <div className=" w-[100%] mx-auto ">
+        <Slider {...settings} className="">
             {
                 media_coverage.map(item=>{
                     return(
-                        <div className="" key={item.id}>
-                            <Link href={item.link} target="_blank">
-                            <div className=" w-[80%] border-2 bg-white p-2 rounded-2xl mb-6 cursor-pointer" >
-                                <Image className="w-[100%] object-cover h-[220px] mb-8" src={item.image} height={300} width={300} alt="News Coverage Image"></Image>
-                                <h1 className="text-[#354895] font-semibold mb-8 text-[16px]">{(item.title).substring(0,65)}...</h1>
-                                <div className="flex-between">
-                                    <div className="w-[50px] h-[50px] p-4">
-                                    <Image src={item.logo} className="h-[100%] w-[100%] object-cover " width={80} height={50} alt="logo"></Image>
-                                    </div>
-                                    <p className="text-[14] fton-semibold text-[#354895]">{item.date}</p>
-                                </div>
-                            </div>
+                        <div className="w-[100%]   " key={item.id}>
+                            <Link href={item.link} target="_blank" classname="">
+                              <div className=" w-[85%]  bg-white p-2 rounded-2xl mb-6 cursor-pointer mx-auto " >
+                                  <Image className="w-[100%] object-cover h-[220px] mb-8" src={item.image} height="100" width="100" alt="News Coverage Image"></Image>
+                                  <h1 className="text-[#354895] font-semibold mb-8 text-[16px]">{(item.title).substring(0,65)}...</h1>
+                                  <div className="flex-between">
+                                      <div className="w-[50px] h-[50px] p-4">
+                                      <Image src={item.logo} className="h-[100%] w-[100%] object-cover " width={80} height={50} alt="logo"></Image>
+                                      </div>
+                                      <p className="text-[14] fton-semibold text-[#354895]">{item.date}</p>
+                                  </div>
+                              </div>
                             </Link>
                             
                         </div>
@@ -64,8 +65,8 @@ const HomePageMediaCoverage = () => {
                 })
             }
         </Slider>
+        </div>
     </div>
- 
     </div>
   )
 }
