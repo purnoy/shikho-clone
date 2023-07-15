0a3b42b1f76dc74330b27585e400fde0378b3fe7
Purnot <purnoy.cc@gmail.com>
2023-07-16 00:28:38 +0600

    added messenger option

--- components/CommonMessaging.js
+++ components/CommonMessaging.js
@@ -1,12 +1,16 @@
 'use client';
 import React, { useState } from 'react';
 import {FaFacebookMessenger, FaChevronDown, FaArrowLeft} from 'react-icons/fa';
-import {BsFillChatRightDotsFill} from "react-icons/bs";
+import {BsFillChatRightDotsFill, BsCheck} from "react-icons/bs";
+import {BiSolidSend} from 'react-icons/bi';
+
 
 
 const CommonMessaging = () => {
     const [textPopUp, setTextPopUp] = useState(false);
     const [chatForm, setChatForm] = useState(false);
+    const [isName, setIsName] = useState(false);
+    const [isNumber, setIsNumber] = useState(false);
 
     const chatboxOpener = ()=>{
         setTextPopUp(true);
@@ -41,11 +45,77 @@ const CommonMessaging = () => {
                     </div>
                 </div>
             </div>
-            <div  className={`${chatForm? 'block h-screen  w-screen bg-yellow-50 sm:h-[450px] sm:w-[380px] absolute -bottom-6 -right-8 sm:bottom-10 sm:right-10 z-40 shadow-2xl sm:rounded-xl ': 'hidden'} `}>
-                <div  className=" ">
-                    <div className="bg-[#354895] w-[100%] rounded-ss-xl rounded-se-xl flex items-center  p-4 text-[14px] text-white">
+            <div  className={`${chatForm? 'block h-screen  w-screen bg-yellow-50 sm:h-[550px] sm:w-[380px] absolute -bottom-6 -right-8 sm:bottom-10 sm:right-10 z-40 shadow-2xl sm:rounded-xl ': 'hidden'} `}>
+                <div className='flex flex-col justify-between h-[100%]'>
+                    <div className=''>
+                        <div className="bg-[#354895] w-[100%] sm:rounded-ss-xl sm:rounded-se-xl flex items-center  p-4 text-[14px] text-white">
                         <FaArrowLeft onClick={()=>setChatForm(false)} className='cursor-pointer text-[22px] me-4' />
-                        <h2 className=' text-white font-bold text-[18px]'>শিখো ডিজিটাল লার্নিং</h2>
+                    <h2 className=' text-white font-bold text-[18px]'>শিখো ডিজিটাল লার্নিং</h2>
+                        </div>       
+
+                        <div className="p-4 m-4 rounded-xl bg-[#ebefff]" >
+                    <p>অনুগ্রহ করে আপনার নাম এবং ফোন নাম্বারটি জানাবেন যেন কোন কারনে আপনি চ্যাট অপশন থেকে চলে গেলে আমরা আপনার সাথে ফলো আপ করতে পারি। 🙂</p>
+                        </div>
+
+                        <div className="m-4 ">
+                            <div className="">
+                            <div className="relative mb-4">
+                                <input type="text" placeholder="নাম" className="w-[100%] outline-none text-[14px] border border-[#354895] rounded-lg p-2"/>
+                                {
+                                isName?(
+                                    <div className="absolute  h-[100%] flex-center cursor-pointer w-[40px] top-0 right-0 rounded-tr-lg rounded-br-lg ">
+                                        <div className="bg-[#354895] h-[30px] w-[30px] rounded-full flex-center ">
+                                            <BsCheck className="text-white text-[24px] "/>
+                                        </div>
+                                    </div>
+                                ): (
+                                    <div onClick={()=>setIsName(true)} className="absolute bg-[#354895] h-[100%] flex-center cursor-pointer w-[40px] top-0 right-0 rounded-tr-lg rounded-br-lg ">
+                                        <BiSolidSend className="text-white text-[24px] "/>
+                                    </div>
+                                )
+                                }
+                            
+                            </div>
+                            {
+                            isName?(
+                                <div className="relative">
+                                <input type="text" placeholder="মোবাইল নম্বর" className="w-[100%] outline-none text-[14px] border border-[#354895] rounded-lg p-2"/>
+                                {
+                                    isNumber?(
+                                    <div className="absolute  h-[100%] flex-center cursor-pointer w-[40px] top-0 right-0 rounded-tr-lg rounded-br-lg ">
+                                        <div className="bg-[#354895] h-[30px] w-[30px] rounded-full flex-center ">
+                                            <BsCheck className="text-white text-[24px] "/>
+                                        </div>
+                                    </div>
+                                    ): (
+                                        <div onClick={()=>setIsNumber(true)} className="absolute bg-[#354895] h-[100%] flex-center cursor-pointer w-[40px] top-0 right-0 rounded-tr-lg rounded-br-lg ">
+                                        <BiSolidSend className="text-white text-[24px] "/>
+                                        </div>
+                                        )
+                                    }
+                            
+                                </div>
+                                ):
+                                (<span></span>)
+                                }
+                            </div>
+                        </div>
+                    </div>
+                    <div className="m-4">
+                        {
+                            isName && isNumber? (
+                                <div className="relative mb-4">
+                                    {/* <input type="text" placeholder="আপনার মেসেজটি লিখুন" className="w-[100%] outline-none text-[14px] border border-[#354895] rounded-lg p-2"/> */}
+                                <textarea placeholder="আপনার মেসেজটি লিখুন" className="w-[100%] h-[100%] outline-none text-[14px] border border-[#354895] rounded-lg p-2"></textarea>
+
+
+                            
+                            </div>
+                            ):
+                            (
+                                <span></span>
+                            )
+                        }
                     </div>
                 </div>
             </div>
