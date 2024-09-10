85894437e740eef1d2f42a07f8c28172c88fdeb9
Minhajul Hoque <90159530+purnoy@users.noreply.github.com>
2024-09-10 11:15:51 +0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,26 @@
 'use client'
-import { useState } from 'react';
+import { useEffect } from 'react';
 import Navbar from './Navbar';
 import Image from 'next/image';
 
-
 const Header = () => {
+  useEffect(() => {
+    // GTM initialization script
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5CHQ7HW4');
+  }, []); // Runs only once after the component mounts
 
   return (
     <header className={`fixed top-0 left-0 right-0 w-screen shadow-lg  bg-white z-40`} >
-        <div className="container mx-auto w-[90%] flex items-center justify-between py-4 px-2 md:px-8">
-            <Image src="https://shikho.com/shikho-logo.svg" alt="Shikho Logo" width={100} height={100}></Image>
-            <Navbar/>
-        </div>
+      <div className="container mx-auto w-[90%] flex items-center justify-between py-4 px-2 md:px-8">
+        <Image src="https://shikho.com/shikho-logo.svg" alt="Shikho Logo" width={100} height={100}></Image>
+        <Navbar/>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
